4873a9562142b11b579c1c1c70ad1c2de0bc4ea9
Pheithar <avalverdemahou@gmail.com>
2023-12-14 20:45:42 +0100

    Initial commit. Testing remotion

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,25 @@ import "../styles/global.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Remotion and Next.js",
-  description: "Remotion and Next.js",
+  title: "Futhark Video Creator",
+  description: "Create videos with translations of the Futhark alphabet, from any given input text.",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"/>
+        <link rel="manifest" href="site.webmanifest"/>
+      </head>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,32 @@
 "use client";
-
+import Grid from '@mui/material/Grid';
 import { Player } from "@remotion/player";
-import type { NextPage } from "next";
-import React, { useMemo, useState } from "react";
-import { Main } from "../remotion/MyComp/Main";
-import {
-  CompositionProps,
-  defaultMyCompProps,
-  DURATION_IN_FRAMES,
-  VIDEO_FPS,
-  VIDEO_HEIGHT,
-  VIDEO_WIDTH,
-} from "../types/constants";
-import { z } from "zod";
-import { RenderControls } from "../components/RenderControls";
-import { Tips } from "../components/Tips/Tips";
-import { Spacing } from "../components/Spacing";
+import { Main as MainComposition } from "@remotion/RunesComposition/Main"
 
-const container: React.CSSProperties = {
-  maxWidth: 768,
-  margin: "auto",
-  marginBottom: 20,
-};
-
-const outer: React.CSSProperties = {
-  borderRadius: "var(--geist-border-radius)",
-  overflow: "hidden",
-  boxShadow: "0 0 200px rgba(0, 0, 0, 0.15)",
-  marginBottom: 40,
-  marginTop: 60,
-};
-
-const player: React.CSSProperties = {
-  width: "100%",
-};
-
-const Home: NextPage = () => {
-  const [text, setText] = useState<string>(defaultMyCompProps.title);
-
-  const inputProps: z.infer<typeof CompositionProps> = useMemo(() => {
-    return {
-      title: text,
-    };
-  }, [text]);
-
+const Home = () => {
   return (
-    <div>
-      <div style={container}>
-        <div className="cinematics" style={outer}>
-          <Player
-            component={Main}
-            inputProps={inputProps}
-            durationInFrames={DURATION_IN_FRAMES}
-            fps={VIDEO_FPS}
-            compositionHeight={VIDEO_HEIGHT}
-            compositionWidth={VIDEO_WIDTH}
-            style={player}
-            controls
-            autoPlay
-            loop
-          />
-        </div>
-        <RenderControls
-          text={text}
-          setText={setText}
-          inputProps={inputProps}
-        ></RenderControls>
-        <Spacing></Spacing>
-        <Spacing></Spacing>
-        <Spacing></Spacing>
-        <Spacing></Spacing>
-        <Tips></Tips>
-      </div>
-    </div>
-  );
-};
+    <Grid container spacing={0}>
+      <Grid item xs={4} className="left-column">
+        <h1>Home</h1>
+      </Grid>
+      <Grid item xs={8} className="right-column">
+        <Player
+          component={MainComposition}
+          durationInFrames={120}
+          compositionWidth={1920}
+          compositionHeight={1080}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          fps={30}
+          controls
+          loop
+          autoplay
+        />
+      </Grid> 
+    </Grid>
+  )
+}
 
 export default Home;
